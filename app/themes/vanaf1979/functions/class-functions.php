c5e92fb4f61482b4b56acf469d4010f0b358db76
Stephan <vanaf1979@gmail.com>
2019-02-07 12:15:51 +0100

    Added global hooks class

--- app/themes/vanaf1979/functions/class-functions.php
+++ app/themes/vanaf1979/functions/class-functions.php
@@ -12,32 +12,19 @@ class Functions
 		$this->theme = wp_get_theme();
 
 		$this->load_dependencies();
-		$this->define_admin_hooks();
 		$this->define_public_hooks();
+		$this->define_admin_hooks();
 	}
 
 
 	private function load_dependencies()
 	{
 		require_once get_template_directory( ) . '/functions/includes/class-functions-loader.php';
-
-        require_once get_template_directory( ) . '/functions/admin/class-functions-admin.php';
-
+		require_once get_template_directory( ) . '/functions/admin/class-functions-global.php';
+		require_once get_template_directory( ) . '/functions/admin/class-functions-admin.php';
 		require_once get_template_directory( ) . '/functions/public/class-functions-public.php';
 
 		$this->loader = new Functions_Loader();
-	}
-
-
-    private function define_admin_hooks()
-	{	
-		if( is_admin() )
-		{
-			$functions_admin = new Functions_Admin( $this->get_theme_name(), $this->get_version() );
-			
-			$this->loader->add_action( 'admin_enqueue_scripts', $functions_admin, 'enqueue_styles' );
-			$this->loader->add_action( 'admin_enqueue_scripts', $functions_admin, 'enqueue_scripts' );
-		}
 	}
 
 
@@ -53,9 +40,21 @@ class Functions
 	}
 
 
-	public function run()
-	{
-		$this->loader->run();
+    private function define_admin_hooks()
+	{	
+		if( is_admin() )
+		{
+			$functions_admin = new Functions_Admin( $this->get_theme_name(), $this->get_version() );
+			
+			$this->loader->add_action( 'admin_enqueue_scripts', $functions_admin, 'enqueue_styles' );
+			$this->loader->add_action( 'admin_enqueue_scripts', $functions_admin, 'enqueue_scripts' );
+		}
+	}
+
+
+	private function define_global_hooks()
+	{	
+		//$functions_admin = new Functions_Admin( $this->get_theme_name(), $this->get_version() );
 	}
 
 
@@ -65,15 +64,21 @@ class Functions
 	}
 
 
+	public function get_version()
+	{
+		return $this->theme->get('Version');
+	}
+
+
 	public function get_loader()
 	{
 		return $this->loader;
 	}
 
 
-	public function get_version()
+	public function run()
 	{
-		return $this->theme->get('Version');
+		$this->loader->run();
 	}
 
 }
