3ff92498521c845fdd872c0b7b0190699a91701b
Stephan <vanaf1979@gmail.com>
2019-02-12 20:42:53 +0100

    Changed slider images to element

--- app/themes/vanaf1979/components/slider.php
+++ app/themes/vanaf1979/components/slider.php
@@ -5,37 +5,75 @@
 
         <div class="swiper-wrapper">
             
-            <div class="swiper-slide" style="background-image: url(/app/themes/vanaf1979/public/img/freestocks-org-505570-unsplash.jpg);">
+            <div class="swiper-slide">
                 
                 <div class="inner va-grid-center">
 
                     <article>
 
                         <h2>Wordpress Dev Monthly Recap for January</h2>
-                        
                         <p>Wordpress articles, tutorials and resources for January 2019</p>
 
                     </article>
 
                 </div>
 
+                <div class="image">
+
+                    <img src="/app/themes/vanaf1979/public/img/freestocks-org-505570-unsplash.jpg" alt="" />
+                    <!-- <img srcset="elva-fairy-568.jpg 568w, elva-fairy-768w.jpg 768w, elva-fairy-1400w.jpg 1400w"
+                        sizes="(max-width: 568px) 100vw, (max-width: 768px) 100vw, 100vw"
+                        src="elva-fairy-800w.jpg" alt="___"> -->
+                </div>
+
             </div>
 
-            <div class="swiper-slide" style="background-image: url(/app/themes/vanaf1979/public/img/raphael-schaller-88763-unsplash.jpg);">
+            <div class="swiper-slide">
                 
                 <div class="inner va-grid-center">
 
                     <article>
 
-                        <h2>TEST Dev Monthly Recap for January</h2>
-                        
+                        <h2>test Dev Monthly Recap for January</h2>
                         <p>Wordpress articles, tutorials and resources for January 2019</p>
 
                     </article>
 
                 </div>
 
+                <div class="image">
+
+                    <img src="/app/themes/vanaf1979/public/img/pankaj-patel-626156-unsplash.jpg" alt="" />
+                    <!-- <img srcset="elva-fairy-568.jpg 568w, elva-fairy-768w.jpg 768w, elva-fairy-1400w.jpg 1400w"
+                        sizes="(max-width: 568px) 100vw, (max-width: 768px) 100vw, 100vw"
+                        src="elva-fairy-800w.jpg" alt="___"> -->
+                </div>
+
             </div>
+
+            <div class="swiper-slide">
+                
+                <div class="inner va-grid-center">
+
+                    <article>
+
+                        <h2>TESR Dev Monthly Recap for January</h2>
+                        <p>Wordpress articles, tutorials and resources for January 2019</p>
+
+                    </article>
+
+                </div>
+
+                <div class="image">
+
+                    <img src="/app/themes/vanaf1979/public/img/raphael-schaller-88763-unsplash.jpg" alt="" />
+                    <!-- <img srcset="elva-fairy-568.jpg 568w, elva-fairy-768w.jpg 768w, elva-fairy-1400w.jpg 1400w"
+                        sizes="(max-width: 568px) 100vw, (max-width: 768px) 100vw, 100vw"
+                        src="elva-fairy-800w.jpg" alt="___"> -->
+                </div>
+
+            </div>
+
         </div>
 
         <div class="swiper-pagination"></div>
