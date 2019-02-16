d87fc0a5f6d109beacaab47c2f6b054858b06aea
Stephan <vanaf1979@gmail.com>
2019-02-16 21:05:38 +0100

    Started with article pages

--- app/themes/vanaf1979/page.php
+++ app/themes/vanaf1979/page.php
@@ -15,23 +15,47 @@
             
             <?php get_template_part('components/breadcrumbs'); ?>
 
-                <div class="writing va-grid-full">
 
-                    <div class="pure-g va-grid-center">
+            <div class="article va-grid-full">
 
-                        <div class="pure-u-1 pure-u-sm-2-3 pure-u-md-2-3 pure-u-lg-2-3 pure-u-xl-2-3">
+                <div class="pure-g va-grid-center">
+
+                    <div class="pure-u-1 pure-u-sm-2-3 pure-u-md-2-3 pure-u-lg-2-3 pure-u-xl-2-3">
+
+                        <article>
 
                             <h1><?php echo $post->post_title; ?></h1>
 
                             <?php echo $theme->get_content( $post->ID ) ?>
 
-                        </div>
+                        </article>
 
-                        <div class="pure-u-1 pure-u-sm-1-3 pure-u-md-1-3 pure-u-lg-1-3 pure-u-xl-1-3">
+                    </div>
+
+                    <div class="sidebar pure-u-1 pure-u-sm-1-3 pure-u-md-1-3 pure-u-lg-1-3 pure-u-xl-1-3">
+
+                        <section>
 
                             <h2>Recent</h2>
 
-                        </div>
+                            <ul>
+                                <li>
+                                    <a href="">WordPress Dev Monthly Recap December</a>
+                                </li>
+                                <li>
+                                    <a href="">WordPress Dev Monthly Recap December</a>
+                                </li>
+                                <li>
+                                    <a href="">WordPress Dev Monthly Recap December</a>
+                                </li>
+                                <li>
+                                    <a href="">WordPress Dev Monthly Recap December</a>
+                                </li>
+                            </ul>
+
+                        </section>
+
+                    </div>
 
                 </div>
 
