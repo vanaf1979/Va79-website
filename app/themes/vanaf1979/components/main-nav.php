ada2aa567dc1f1b5da192da569a2671ed0e77ed1
Stephan <vanaf1979@gmail.com>
2019-03-03 20:32:05 +0100

    Refactored main nav to scss

--- app/themes/vanaf1979/components/main-nav.php
+++ app/themes/vanaf1979/components/main-nav.php
@@ -11,12 +11,12 @@
         <div class="nav pure-u-1 pure-u-sm-3-4 pure-u-md-3-4 pure-u-lg-3-4 pure-u-xl-3-4">
             
             <?php //wp_page_menu('show_home=Home&menu_class=mainmenu&sort_column=menu_order&exclude=1&depth=1&exclude_tree=1'); ?>
-            <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
+            <?php wp_nav_menu( array( 'menu_class' => 'menu hor right' , 'theme_location' => 'header-menu' ) ); ?>
 
             <?php 
             $lang = pll_the_languages(array('raw'=>1));
             ?>
-            <ul class="lang">
+            <ul class="lang menu hor right">
                 <li>
                     <a href="<?php echo $lang['nl']['url']; ?>">Dutch</a>
                 </li>
